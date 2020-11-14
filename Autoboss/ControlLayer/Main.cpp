41c7586876c76eb5fa7c27591f8cc66721c57f64
chrinchin <48599958+chrinchin@users.noreply.github.com>
2020-11-13 18:26:33 -0800

    hopefully this helps

--- Autoboss/ControlLayer/Main.cpp
+++ Autoboss/ControlLayer/Main.cpp
@@ -3,6 +3,12 @@
 #include "Main.h"
 #include "BusinessCtr.h"
 #include "DatabaseCtr.h"
+#include <afxwin.h>
+
+class CExample : public CWinApp {
+public:
+	BOOL InitInstance();
+};
 
 int main(int argc, char* argv[]) {
 	return 0;

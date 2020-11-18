7f1785d9551fa86ed6a70a83790f2d25e0d306a1
chrinchin <48599958+chrinchin@users.noreply.github.com>
2020-11-18 01:46:44 -0800

    Wrote some sudo code

--- Autoboss/ControlLayer/Main.cpp
+++ Autoboss/ControlLayer/Main.cpp
@@ -9,6 +9,38 @@ int main(int argc, char* argv[]) {
 	Main controller;
 
 	controller.InstantiateControllers();
+
+	// sudo code : the story of main (short story)
+	/*
+	* Connect to database //prequel
+	* Get date
+	* apply date to database
+	* Get password from database
+	* if (password is null):
+	*	Display create password window
+	* else:
+	*	Display enter password window
+	* Display main menu
+	* while user does not exit:
+	*	case user selects warehouse:
+	*		Get warehouses from database
+	*		Display warehouse selection
+	*			case user selects select warehouse:
+	*				Get inventory for said warehouse from database
+	*				Display Inventory
+	*			case user selects add new warehouse:
+	*			case user selects back:
+	*				return
+	*	case user selects warnings:
+	*	case user selects oInvoice:
+	*	case user selects cInvoice:
+	*	case user selects create product:
+	*	case user selects product stats:
+	*	case user selects commissions:
+	*	case user selects settings:
+	* Disconnect Database
+	*/
+
 	controller.CloseControllers();
 
 	return 0;

2f79900f43f061fae72158dcc343539b2a4b2893
chrinchin <48599958+chrinchin@users.noreply.github.com>
2020-11-21 12:59:50 -0800

    Unfortunately, everyone is going to have to download and install wxwidgets

--- Autoboss/GUILayer/GUIHelper.cpp
+++ Autoboss/GUILayer/GUIHelper.cpp
@@ -1,27 +1,26 @@
 #include "GUIHelper.h"
-#include <afxwin.h>
 
 using namespace GUILayer;
 
-class CMyFrame : public CFrameWnd {
-   public:
-      CMyFrame() {
-         Create(NULL, _T("MFC Application Tutorial"), WS_SYSMENU, CRect(90, 120, 550, 480));
-      }
-};
-
-class CExample : public CWinApp {
-   BOOL InitInstance() {
-      CMyFrame *Frame = new CMyFrame();
-      m_pMainWnd = Frame;
-      
-      Frame->ShowWindow(SW_NORMAL);
-      Frame->UpdateWindow();
-      
-      return TRUE;
-   }
-};
+//class CMyFrame : public CFrameWnd {
+//   public:
+//      CMyFrame() {
+//         Create(NULL, _T("MFC Application Tutorial"), WS_SYSMENU, CRect(90, 120, 550, 480));
+//      }
+//};
+//
+//class CExample : public CWinApp {
+//   BOOL InitInstance() {
+//      CMyFrame *Frame = new CMyFrame();
+//      m_pMainWnd = Frame;
+//      
+//      Frame->ShowWindow(SW_NORMAL);
+//      Frame->UpdateWindow();
+//      
+//      return TRUE;
+//   }
+//};
 
 void GUIHelper::test() {
-    CExample theApp;
+    //CExample theApp;
 }

b626c737cad2ade000b9ef64def4590ffc5b071c
Brenda Solis <brendab4solis@gmail.com>
2020-11-27 15:45:57 -0800

    Adding warehouses
    Comments made on the password ui only to improve readability

--- Autoboss/GUILayer/Warehouse_ui.h
+++ Autoboss/GUILayer/Warehouse_ui.h
@@ -7,32 +7,14 @@
 
 #ifndef WAREHOUSE_UI_H
 namespace GUILayer {
+	//Adding a warehouse
 	class AddWarehouse : public wxFrame {
 	private:
 	public:
-		AddWarehouse();
-		enum {
-			EnterNameID = 004,
-			AddressID = 005,
-			CityID = 006,
-			EnterZipCodeID = 007
-		};
-
-		// Sizers
-		wxBoxSizer* sizerH = nullptr;
-		wxBoxSizer* sizerV = nullptr;
-		// widgets
-		wxButton* addWH = nullptr;
-		wxTextCtrl* enterAddressTxtBox = nullptr;
-		wxTextCtrl* enterNameTextBox = nullptr;
-		wxTextCtrl* enterZipCodeTextBox = nullptr;
-		wxTextCtrl* CityTextBox = nullptr;
-		wxStaticText* enterAddress = nullptr;
-		wxStaticText* enterName = nullptr;
-		wxStaticText* enterCity = nullptr;
-		wxStaticText* enterZipcode = nullptr;
-		wxStaticText* addWareHouse = nullptr;
+	
 	};
+
+	//adding 
 }
 #define WAREHOUSE_UI_H
 #endif

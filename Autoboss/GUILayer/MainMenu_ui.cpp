6684dcb0518342d924e30f5cf8e4f0b51e8cf854
Brenda Solis <brendab4solis@gmail.com>
2020-11-28 14:12:04 -0800

    The WareHouse Selection
    Warehouse Selection and the MainMenu

--- Autoboss/GUILayer/MainMenu_ui.cpp
+++ Autoboss/GUILayer/MainMenu_ui.cpp
@@ -5,7 +5,29 @@
 #endif
 
 using namespace GUILayer;
+using namespace std;
+//<<<<<<< Updated upstream
 
 MainMenu_ui::MainMenu_ui() 
-{}}
+{}
+//=======
+using namespace std;
+
+/**/
+MainMenu_ui::MainMenu_ui()
+{}
+
+void MainMenu_ui::Display()
+{
+	
+	cout << "******************************************************" << endl;
+	cout << "**********************Main Menu***********************" << endl;
+	cout << "1. Commission Display			2. Warehouse Selection " << endl;
+	cout << "3. Product Statistics			4. Warnings" << endl;
+	cout << "5. Create Product				6. Create Product" << endl;
+	cout << "7. Update Product				8. Closed Invoice List" << endl;
+	cout << "******************************************************" << endl;
+	cout << "\n Choice Selection:";
+	
+}
 >>>>>>> Stashed changes
--- Autoboss/GUILayer/Warehouse_ui.cpp
+++ Autoboss/GUILayer/Warehouse_ui.cpp
@@ -1,9 +1,21 @@
 // Warehouse_ui.cpp
 #ifndef WAREHOUSE_UI_H
 #include "Warehouse_ui.h"
+#include<iostream>
+#include<vector>
 #endif
 
 using namespace GUILayer;
-
+using namespace std;
 Warehouse_ui::Warehouse_ui()
 {}
+void Warehouse_selection(vector<int> WareHouses )
+{
+	cout << "******************************************************" << endl;
+	cout << "**************Warehouse Selection Menu****************" << endl;
+	for (int i = 0; i < WareHouses.size(); i++)
+	{
+		cout << "WareHouse " + (i+1) << endl;
+	}
+	cout << "Choose your warehouse:" << endl;
+}
